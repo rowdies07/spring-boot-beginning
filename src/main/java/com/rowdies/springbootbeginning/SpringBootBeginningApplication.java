0261d010e87a4c9f7e76ed3adf0537a109723df5
Ravindra <55906895+rowdies07@users.noreply.github.com>
2023-01-10 03:12:08 +0530

    Initial commit

---
+++ src/main/java/com/rowdies/springbootbeginning/SpringBootBeginningApplication.java
@@ -0,0 +1,13 @@
+package com.rowdies.springbootbeginning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeginningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBeginningApplication.class, args);
+	}
+
+}
